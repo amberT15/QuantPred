7ac3f386dea7ef2d580e6ea5cb85a5216ce29f62
shtoneyan <tikinbobo@gmail.com>
2021-02-02 14:31:53 -0500

    add download command

--- create_small_datasets.sh
+++ create_small_datasets.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-./filter_encode_data.py /home/shush/profile/QuantPred/datasets/QQ_encode_TF.txt datasets/ -b HepG2 --no_crispr -l 7
 
-./filter_encode_data.py /home/shush/profile/QuantPred/datasets/QQ_encode_TF.txt datasets/ -b K562 --no_crispr -l 7
+# home/shush/profile/QuantPred/filter_encode_data.py /home/shush/profile/QuantPred/datasets/QQ_encode_TF.txt datasets/ -b HepG2 --no_crispr -l 7
+
+# home/shush/profile/QuantPred/filter_encode_data.py /home/shush/profile/QuantPred/datasets/QQ_encode_TF.txt datasets/ -b K562 --no_crispr -l 7
+
+
+/home/shush/profile/QuantPred/download_urls.py /home/shush/profile/QuantPred/datasets/HepG2_urls.txt /home/shush/profile/QuantPred/datasets/HepG2_bedfiles
+/home/shush/profile/QuantPred/download_urls.py /home/shush/profile/QuantPred/datasets/K562_urls.txt /home/shush/profile/QuantPred/datasets/K562_bedfiles
