dea3b948941a160841c2993e170de23e5b168994
Ziqi (Amber) Tang <amber.zqt@gmail.com>
2021-02-03 12:19:09 -0500

    modified path in create_small_datsets.sh into relative path

--- create_small_datasets.sh
+++ create_small_datasets.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 
-home/shush/profile/QuantPred/filter_encode_data.py /home/shush/profile/QuantPred/datasets/QQ_encode_TF.txt datasets/ -b HepG2 --no_crispr -l 7
+./filter_encode_data.py ./datasets/QQ_encode_TF.txt datasets/ -b HepG2 --no_crispr -l 7
 
-home/shush/profile/QuantPred/filter_encode_data.py /home/shush/profile/QuantPred/datasets/QQ_encode_TF.txt datasets/ -b K562 --no_crispr -l 7
+./filter_encode_data.py./datasets/QQ_encode_TF.txt datasets/ -b K562 --no_crispr -l 7
 
 
-/home/shush/profile/QuantPred/download_urls.py /home/shush/profile/QuantPred/datasets/HepG2_urls.txt /home/shush/profile/QuantPred/datasets/HepG2_bedfiles
-/home/shush/profile/QuantPred/download_urls.py /home/shush/profile/QuantPred/datasets/K562_urls.txt /home/shush/profile/QuantPred/datasets/K562_bedfiles
+./download_urls.py ./datasets/HepG2_urls.txt HepG2_bedfiles
+./download_urls.py ./datasets/K562_urls.txt K562_bedfiles
