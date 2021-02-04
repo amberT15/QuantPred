15baff32678d2ca872e5c6b39b611109d4cb7f88
Ziqi (Amber) Tang <amber.zqt@gmail.com>
2021-02-04 15:52:36 -0500

    add K562 bash command

--- create_bed.sh
+++ create_bed.sh
@@ -2,4 +2,4 @@
 
 python ./bed_generation.py -m 200 -s 1000 -o HepG2_small_dataset -c ./datasets/hg19.chrom.sizes ./datasets/HepG2/HepG2_sample_beds.txt
 
-#python ./bed_generation.py -m 200 -s 1000 -o K562_small_dataset -c ./datasets/hg19.chrom.sizes ./datasets/K562_sample_beds.txt
+python ./bed_generation.py -m 200 -s 1000 -o K562_small_dataset -c ./datasets/hg19.chrom.sizes ./datasets/K562/K562_sample_beds.txt
