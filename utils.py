606481da64c6993cc6bd89835fae0d768347d395
shtoneyan <tikinbobo@gmail.com>
2021-02-10 11:26:29 -0500

    scrip to collect helper functions

--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
-
+import urllib.request
 import sys
 import os
+import pandas as pd
 from shutil import copyfile
 from os.path import dirname
 
@@ -22,3 +23,13 @@ def make_directory(path):
 
 def get_parent(filepath):
     return dirname(filepath)
+
+
+def download_metadata(metadata_url, output_folder):
+    print('~~~~~~~~~~~~')
+    print(output_folder)
+    print("Downloading metadata.tsv for the project")
+    metadata_path = os.path.join(output_folder, 'metadata.tsv')
+    urllib.request.urlretrieve(metadata_url, metadata_path)
+    metadata = pd.read_csv(metadata_path ,sep='\t')
+    return metadata
