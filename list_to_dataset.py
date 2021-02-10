fd812a2614c8205becf40a7aaf9ffd5b16a38cda
shtoneyan <tikinbobo@gmail.com>
2021-02-10 16:34:14 -0500

    modify script to have 4 output folders and a summary.csv file

--- list_to_dataset.py
+++ list_to_dataset.py
@@ -11,6 +11,19 @@ def make_dir(outdir):
     os.mkdir(outdir)
   return(outdir)
 
+def make_label(df_row):
+
+    '''
+    Add label for each row selected:
+    Assay_
+    Experiment target_
+    Biosample term name_
+    Experiment accession_
+    '''
+    label_list = [str(c.values[0]) for c in [df_row['Assay'], df_row['Experiment target'], df_row['Biosample term name'],
+                         df_row['Experiment accession']]]
+    return('_'.join(label_list).replace(" ", "-"))
+
 def get_best(df):
   l_bio_reps = [len(l) for l in df['Biological replicate(s)'].values]
   all_same = all(element == l_bio_reps[0] for element in l_bio_reps)
@@ -21,26 +34,28 @@ def get_best(df):
     i_best = np.argmax(l_bio_reps)
     return i_best
 
-def process_exp(exp_accession, metadata, assembly, res_dict):
+def process_exp(exp_accession, metadata, assembly):
   exp_df = metadata[(metadata['Experiment accession']==exp_accession) & (metadata['File assembly']==assembly)]
   assert exp_df.size > 0, 'Bad accession number, no records found'
 
-  bed = exp_df[exp_df['File format'] == 'bigBed narrowPeak']
-  c1 = bed['Output type']=='conservative IDR thresholded peaks'
-  c2 = exp_df['Output type'] == 'signal p-value'
-  c3 = exp_df['Output type'] == 'fold change over control'
-  c4 = exp_df['Output type'] == 'alignments'
-  assert any(c1) and any(c2) and any(c3) and any(c4), exp_accession+' has missing data types'
-  bed = bed[c1]
+  bed = exp_df[(exp_df['File format'] == 'bigBed narrowPeak') &
+                (exp_df['Output type']=='conservative IDR thresholded peaks')]
+  sign = exp_df[exp_df['Output type'] == 'signal p-value'] #check if signal bw exists
+  fold = exp_df[exp_df['Output type'] == 'fold change over control'] #check if fold bw exists
+  bam = exp_df[exp_df['Output type'] == 'alignments'] #check if bam exists
+  # throw an error if any one absent
+  outputs = [bed, sign, fold, bam]
+  assert all([i.shape[0]>0 for i in outputs]), exp_accession+' has missing data types'
+  # check if multiple exist, which there shouldn't be?
   assert bed.shape[0] == 1, 'Multiple conservative IDR thresholded peak bed files identified'
-  res_dict['bed'].append(bed)
-  sign_bws = exp_df[c2]
-  res_dict['sign'].append(sign_bws.iloc[get_best(sign_bws),:])
-  fold_bws = exp_df[c3]
-  res_dict['fold'].append(fold_bws.iloc[get_best(fold_bws),:])
-  # sign_best = get_best(sign_bws)
-  bam = exp_df[c4]
-  res_dict['bam'].append(bam)
+  sign = sign.iloc[[get_best(sign)]]
+  fold = fold.iloc[[get_best(fold)]]
+  bam = bam.iloc[[0]]
+  summary_line = [make_label(bed)]
+  summary_line = summary_line + [i['File download URL'].values[0] for i in outputs]
+  return summary_line
+
+
 
 def wget_list(urls, outdir):
   for url in urls:
@@ -48,40 +63,47 @@ def wget_list(urls, outdir):
     h.close()
 
 def save_dataset(res_dict, outdir):
-    for prefix, filtered_list in res_dict.items():
-        df = pd.concat(filtered_list, axis=1).T
-        df.to_csv(os.path.join(outdir, '{}.csv'.format(prefix)))
-        prefix_dir = make_dir(os.path.join(outdir, prefix))
-        urls = df['File download URL'].values
-        wget_list(urls, prefix_dir)
+  for prefix, filtered_list in res_dict.items():
+    print("Prcessing set labelled {}".format(prefix))
+    df = pd.concat(filtered_list, axis=1)
+    prefix_dir = make_dir(os.path.join(outdir, prefix))
+    df.to_csv(os.path.join(prefix_dir, '{}.csv'.format(prefix)))
 
-def create_dataset(exp_accession_list, outdir):
+    urls = df['File download URL'].values
+
+    wget_list(urls, prefix_dir)
+
+def create_dataset(exp_accession_list, outdir, folder_label='summary'):
+  cols = ['label','bed', 'sign', 'fold', 'bam']
+  summary = []
   if not os.path.isdir(outdir):
     os.mkdir(outdir)
   assembly = 'GRCh38'
-  res_dict = {'sign':[], 'fold':[], 'bam':[], 'bed':[]}
   for exp_accession in exp_accession_list:
-    process_exp(exp_accession, metadata, assembly, res_dict)
-  save_dataset(res_dict, outdir)
+    summary.append(process_exp(exp_accession, metadata, assembly))
+  sum_df = pd.DataFrame(summary, columns=cols)
+  sum_df.to_csv(os.path.join(outdir, folder_label+'.csv'))
+  for i in range(1, len(cols)):
+      data_subdir = make_dir(os.path.join(outdir, cols[i]))
+      wget_list(sum_df[cols[i]].values, data_subdir)
 
 
 
-# files_path = './datasets/QQ_encode_TF.txt'
-# output_folder = './datasets'
-#
-# with open(files_path, "r") as file:
-#     metadata_url = file.readline()[1:-2] #remove " before and after url
-# # file list metadata
-# metadata = utils.download_metadata(metadata_url, output_folder)
-metadata_path = 'datasets/metadata.tsv'
+files_path = 'datasets/QQ_encode_TF.txt'
+output_folder = 'datasets'
+
+with open(files_path, "r") as file:
+    metadata_url = file.readline()[1:-2] #remove " before and after url
+# file list metadata
+metadata = utils.download_metadata(metadata_url, output_folder)
 metadata = pd.read_csv(metadata_path, sep='\t')
 
 
-exp_accession_list_A549 = ['ENCSR544GUO']
-# exp_accession_list_A549 = ['ENCSR544GUO', 'ENCSR000BUB', 'ENCSR035OXA', 'ENCSR886OEO', 'ENCSR593DGU', 'ENCSR192PBJ', 'ENCSR979IOT']
+# exp_accession_list_A549 = ['ENCSR544GUO', 'ENCSR000BUB']
+exp_accession_list_A549 = ['ENCSR544GUO', 'ENCSR000BUB', 'ENCSR035OXA', 'ENCSR886OEO', 'ENCSR593DGU', 'ENCSR192PBJ', 'ENCSR979IOT']
 outdir_A549 = 'datasets/A549'
 create_dataset(exp_accession_list_A549, outdir_A549)
 
 # exp_accession_list_HepG2 = ['ENCSR544GUO', 'ENCSR000BUB', 'ENCSR035OXA', 'ENCSR886OEO', 'ENCSR593DGU', 'ENCSR192PBJ', 'ENCSR979IOT']
-# outdir_HepG2 = 'HepG2'
+# outdir_HepG2 = 'datasets/HepG2'
 # create_dataset(exp_accession_list_HepG2, outdir_HepG2)
