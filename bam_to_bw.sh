6737a1873b85ba9a144e1ed6a5a3eb3067f9bb99
shtoneyan <tikinbobo@gmail.com>
2021-02-11 09:22:11 -0500

    add loop over all bam files in a folder

--- bam_to_bw.sh
+++ bam_to_bw.sh
@@ -1,11 +1,37 @@
 #!/bin/bash
-filename=$1
-ref=$2
-samtools sort $filename.bam -o $filename.sorted.bam
-bedtools genomecov -ibam $filename.sorted.bam -bg > $filename.bedGraph
-LC_COLLATE=C sort -k1,1 -k2,2n $filename.bedGraph > $filename.sorted.bedGraph
-bedGraphToBigWig $filename.sorted.bedGraph $2 $filename.bw
+# filepath=$1
+# ref=$2
+# samtools sort $filepath.bam -o $filepath.sorted.bam
+# bedtools genomecov -ibam $filepath.sorted.bam -bg > $filepath.bedGraph
+# LC_COLLATE=C sort -k1,1 -k2,2n $filepath.bedGraph > $filepath.sorted.bedGraph
+# bedGraphToBigWig $filepath.sorted.bedGraph $2 $filepath.bw
+#
+# rm $filepath.sorted.bam
+# rm $filepath.bedGraph
+# rm $filepath.sorted.bedGraph
 
-rm $filename.sorted.bam
-rm $filename.bedGraph
-rm $filename.sorted.bedGraph
+
+folder_path=$1
+ref=$2
+output_dir=$3
+
+for FILE in $folder_path*.bam; do
+  [ -f "$FILE" ] || break
+
+  # echo Processing $FILE
+  filepath="${FILE%.*}"
+  exp_id="${filepath##*/}"
+  output_path="$output_dir/$exp_id.bw"
+  echo Sorting bam file $FILE
+  samtools sort $filepath.bam -o $filepath.sorted.bam
+  echo Converting bam to bedGraph
+  bedtools genomecov -ibam $filepath.sorted.bam -bg > $filepath.bedGraph
+  LC_COLLATE=C sort -k1,1 -k2,2n $filepath.bedGraph > $filepath.sorted.bedGraph
+  echo Converting bedGraph to bigwig
+  bedGraphToBigWig $filepath.sorted.bedGraph $ref $output_path
+
+  rm $filepath.sorted.bam
+  rm $filepath.bedGraph
+  rm $filepath.sorted.bedGraph
+
+done
