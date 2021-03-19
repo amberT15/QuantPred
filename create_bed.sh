5f386d358cfae27c0b637782e55d0b809186cc66
shtoneyan <tikinbobo@gmail.com>
2021-03-19 11:11:42 -0400

    archive 7 TF dataset scripts

--- create_bed.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python ./bed_generation.py -m 200 -s 1000 -o HepG2_small_dataset -c ./datasets/hg19.chrom.sizes ./datasets/HepG2/HepG2_sample_beds.txt
-
-python ./bed_generation.py -m 200 -s 1000 -o K562_small_dataset -c ./datasets/hg19.chrom.sizes ./datasets/K562/K562_sample_beds.txt
