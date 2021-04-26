01ae6e0939d74cacf91e55cb9354e3bcb0351930
shtoneyan <tikinbobo@gmail.com>
2021-04-26 14:03:03 -0400

    add comments

--- datasets/create_25TF_datasets.py
+++ datasets/create_25TF_datasets.py
@@ -3,25 +3,26 @@ import subprocess
 import yaml, os, shutil
 
 # parameter grid to loop over
-# pool_window = [1, 32, 64, 128, 256]
-# input_size = [1024, 2048, 3072]
+pool_window = [1, 32, 64, 128, 256]
+input_size = [1024, 2048, 3072]
+# i = 2048
+# p = 64
 
-p = 64
-i = 512
+for i in input_size:
+    for p in pool_window:
+        print('Creating dataset using input size %i and pool window %i' % (i, p))
+        template_path = r'top25/template_config.yaml'
 
-template_path = r'top25/template_config.yaml'
 
-
-with open(template_path) as file:
-    config = yaml.load(file, Loader=yaml.FullLoader)
-#update parameters
-config['input']['size'] = i
-config['input']['pool'] = p
-config['output']['prefix'] = 'i_%i_w_%i' % (config['input']['size'], config['input']['pool'])
-# loops
-# for p in pool_window:
-#     for i in input_size:
-current_config = 'config.yaml'
-with open(current_config, 'w') as file:
-    documents = yaml.dump(config, file)
-subprocess.call('./bw_to_tfr.sh')
+        with open(template_path) as file:
+            config = yaml.load(file, Loader=yaml.FullLoader)
+        #update parameters
+        config['input']['size'] = i
+        config['input']['pool'] = p
+        config['output']['prefix'] = 'i_%i_w_%i' % (config['input']['size'], config['input']['pool'])
+        current_config = 'config.yaml'
+        #save as the config file to be used in running the preprocessing
+        with open(current_config, 'w') as file:
+            documents = yaml.dump(config, file)
+        # run dataset generation script with current parameters
+        subprocess.call('./bw_to_tfr.sh')
