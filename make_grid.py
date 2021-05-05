240490e9a94e9f3858fbfd4f8ed065cae9650b45
shtoneyan <tikinbobo@gmail.com>
2021-05-05 13:42:20 -0400

    fix file and epoch options

--- make_grid.py
+++ make_grid.py
@@ -2,13 +2,13 @@
 import os
 
 all_data = '/home/shush/profile/QuantPred/datasets/top25/'
-output_file = 'test.txt'
+output_file = 'commands.txt'
+epoch_n = 2
 # define grid points
 
 # inputs
 inputs = [os.path.join(all_data, foldername) for foldername in os.listdir(all_data) if 'w_32' in foldername]
-print('Add inputs in directories')
-print(inputs)
+
 
 # model
 
@@ -20,13 +20,18 @@ losses = ['poisson', 'mse', 'pearsonr', 'fft_mse_loss']
 
 cmd = []
 # loop and save commands to a list
+output_dir = 'test_dir'
 for input in inputs:
     for model in model_names:
         for loss in losses:
-            cmd.append('./train.py {} {} {} -e 1 -o test_run'.format(input, model, loss))
+            cmd.append('./train.py {} {} {} -e {} -o {}'.format(input, model, loss, epoch_n, output_dir))
+
+# add more loops here
+
+
 
 
 # save list to file
-with open(output_file, 'a') as f:
+with open(output_file, 'w') as f:
     for command in cmd:
         f.write("%s\n" % command)
