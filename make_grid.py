e3e5680137b45199db5c9211f5c5886bc177abd9
shtoneyan <tikinbobo@gmail.com>
2021-05-12 11:27:19 -0400

    change paths for new grid search

--- make_grid.py
+++ make_grid.py
@@ -2,38 +2,39 @@
 import os
 
 all_data = '/home/shush/profile/QuantPred/datasets/top25/'
-output_file = 'commands_grid1.txt'
+output_file = 'commands_grid2.txt'
 epoch_n = 100
 # define grid points
 
 # inputs
 inputs_all = [os.path.join(all_data, foldername) for foldername in os.listdir(all_data) if 'w_' in foldername]
 inputs_basenji = [os.path.join(all_data, foldername) for foldername in os.listdir(all_data) if 'w_32' in foldername]
-inputs_bpnet = [os.path.join(all_data, foldername) for foldername in os.listdir(all_data) if 'w_32' in foldername]
+inputs_bpnet = [os.path.join(all_data, foldername) for foldername in os.listdir(all_data) if 'w_1' in foldername and 'w_128' not in foldername]
 
 # model
 
 
 # losses
 
-losses = ['poisson', 'mse', 'pearsonr', 'fft_mse', 'multinomial_nll']
+losses = ['poisson', 'mse', 'basenjipearsonr', 'fftmse', 'multinomialnll']
 
 cmd = []
 # loop and save commands to a list
-output_dir = '/home/shush/profile/QuantPred/datasets/top25/grid1'
+output_dir = '/home/shush/profile/QuantPred/datasets/top25/grid2'
 for input in inputs_basenji:
     for loss in losses:
-        cmd.append('./train.py {} basenji_small {} -e {} -o {}'.format(input, loss, epoch_n, output_dir))
+        cmd.append('./train_bas.py {} basenji {} -e {} -o {} '.format(input, loss, epoch_n, output_dir))
 
 # add more loops here
 for input in inputs_bpnet:
     for loss in losses:
-        cmd.append('./train.py {} bpnet {} -e {} -o {}'.format(input, loss, epoch_n, output_dir))
+        if loss != 'poisson':
+            cmd.append('./train_bas.py {} bpnet {} -e {} -o {}'.format(input, loss, epoch_n, output_dir))
 
 # add more loops here
 for input in inputs_all:
     for loss in losses:
-        cmd.append('./train.py {} custom_lstm {} -e {} -o {}'.format(input, loss, epoch_n, output_dir))
+        cmd.append('./train_bas.py {} lstm {} -e {} -o {}'.format(input, loss, epoch_n, output_dir))
 
 # save list to file
 with open(output_file, 'w') as f:
