e19665ed14a2a47242d883e1475e932bf3b7298d
shtoneyan <tikinbobo@gmail.com>
2021-05-12 11:28:53 -0400

    add functions for pearson r and functions for fft calculation with no memory issues

--- metrics.py
+++ metrics.py
@@ -10,15 +10,27 @@ from scipy.stats import pearsonr
 import seaborn as sns
 import pandas as pd
 import scipy
+from scipy.fft import fft
 import sklearn.metrics as skm
+from tensorflow.python.keras import backend as K
 
 
 def metrify(func):
+    '''Wrapper for getting per TF metric from TF losses'''
     def wrapper(y_true,y_pred):
         y_true = tf.expand_dims(y_true, axis=-1)
         y_pred = tf.expand_dims(y_pred, axis=-1)
         return func(y_true,y_pred)
     return wrapper
+
+
+def metric_fftmse(y_true,y_pred):
+    '''MSE(FFT) using scipy for not runnign into memory issues during evaluation'''
+    return np.mean(np.mean(np.square((scipy.fft.fft(y_true)-scipy.fft.fft(y_pred)).astype('float')), axis=0), axis=0)
+
+def metric_fftabs(y_true,y_pred):
+    '''Abs(FFT)'''
+    return np.mean((np.abs((scipy.fft.fft(y_true)-scipy.fft.fft(y_pred)).astype('float'), axis=0)))
 
 def metric_pearsonr(y, pred):
     y_true = tf.cast(y, 'float32')
@@ -104,18 +116,19 @@ def pearsonr_per_seq(y, pred):
     pred_var = pred_sumsq - tf.multiply(count, pred_mean2)
     tp_var = tf.multiply(tf.math.sqrt(true_var), tf.math.sqrt(pred_var))
     correlation = tf.divide(covariance, tp_var)
+    nonan_corr = np.nanmean(correlation.numpy(), axis=0)
+    # nonan_corr = tf.boolean_mask(correlation, tf.math.is_finite(correlation))
+    return nonan_corr
 
-    return tf.reduce_mean(correlation)
 
-
-def calculate_pearsonr(target,pred):
-    pearson_profile =np.zeros((target.shape[2],len(target)))
-
-    for task_i in range(0,target.shape[2]):
-        for sample_i in range(0,len(target)):
-            pearson_profile[task_i,sample_i]=(pearsonr(target[sample_i][:,task_i],pred[sample_i][:,task_i])[0])
-
-    return pearson_profile
+# def calculate_pearsonr(target,pred):
+#     pearson_profile =np.zeros((target.shape[2],len(target)))
+#
+#     for task_i in range(0,target.shape[2]):
+#         for sample_i in range(0,len(target)):
+#             pearson_profile[task_i,sample_i]=(pearsonr(target[sample_i][:,task_i],pred[sample_i][:,task_i])[0])
+#
+#     return pearson_profile
 
 
 def pearson_volin(pearson_profile,tasks,figsize=(20,5)):
@@ -271,3 +284,64 @@ def eval_profile(yt, yp,
                     })
 
     return pd.DataFrame.from_dict(out)
+
+class PearsonR(tf.keras.metrics.Metric):
+  def __init__(self, num_targets, summarize=True, name='pearsonr', **kwargs):
+    super(PearsonR, self).__init__(name=name, **kwargs)
+    self._summarize = summarize
+    self._shape = (num_targets,)
+    self._count = self.add_weight(name='count', shape=self._shape, initializer='zeros')
+
+    self._product = self.add_weight(name='product', shape=self._shape, initializer='zeros')
+    self._true_sum = self.add_weight(name='true_sum', shape=self._shape, initializer='zeros')
+    self._true_sumsq = self.add_weight(name='true_sumsq', shape=self._shape, initializer='zeros')
+    self._pred_sum = self.add_weight(name='pred_sum', shape=self._shape, initializer='zeros')
+    self._pred_sumsq = self.add_weight(name='pred_sumsq', shape=self._shape, initializer='zeros')
+
+  def update_state(self, y_true, y_pred, sample_weight=None):
+    y_true = tf.cast(y_true, 'float32')
+    y_pred = tf.cast(y_pred, 'float32')
+
+    product = tf.reduce_sum(tf.multiply(y_true, y_pred), axis=[0,1])
+    self._product.assign_add(product)
+
+    true_sum = tf.reduce_sum(y_true, axis=[0,1])
+    self._true_sum.assign_add(true_sum)
+
+    true_sumsq = tf.reduce_sum(tf.math.square(y_true), axis=[0,1])
+    self._true_sumsq.assign_add(true_sumsq)
+
+    pred_sum = tf.reduce_sum(y_pred, axis=[0,1])
+    self._pred_sum.assign_add(pred_sum)
+
+    pred_sumsq = tf.reduce_sum(tf.math.square(y_pred), axis=[0,1])
+    self._pred_sumsq.assign_add(pred_sumsq)
+
+    count = tf.ones_like(y_true)
+    count = tf.reduce_sum(count, axis=[0,1])
+    self._count.assign_add(count)
+
+  def result(self):
+    true_mean = tf.divide(self._true_sum, self._count)
+    true_mean2 = tf.math.square(true_mean)
+    pred_mean = tf.divide(self._pred_sum, self._count)
+    pred_mean2 = tf.math.square(pred_mean)
+
+    term1 = self._product
+    term2 = -tf.multiply(true_mean, self._pred_sum)
+    term3 = -tf.multiply(pred_mean, self._true_sum)
+    term4 = tf.multiply(self._count, tf.multiply(true_mean, pred_mean))
+    covariance = term1 + term2 + term3 + term4
+
+    true_var = self._true_sumsq - tf.multiply(self._count, true_mean2)
+    pred_var = self._pred_sumsq - tf.multiply(self._count, pred_mean2)
+    tp_var = tf.multiply(tf.math.sqrt(true_var), tf.math.sqrt(pred_var))
+    correlation = tf.divide(covariance, tp_var)
+
+    if self._summarize:
+        return tf.reduce_mean(correlation)
+    else:
+        return correlation
+
+  def reset_states(self):
+      K.batch_set_value([(v, np.zeros(self._shape)) for v in self.variables])
