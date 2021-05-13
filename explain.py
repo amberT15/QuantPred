922ff49a66f95d605c0f8b6eee89adf4304315e5
Ziqi (Amber) Tang <amber.zqt@gmail.com>
2021-05-13 14:53:10 -0400

    create saliency function script

---
+++ explain.py
@@ -0,0 +1,24 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+
+@tf.function
+def peak_saliency_map(X, model, class_index, func=tf.math.reduce_mean):
+    """fast function to generate saliency maps"""
+    if not tf.is_tensor(X):
+        X = tf.Variable(X)
+
+    with tf.GradientTape() as tape:
+        tape.watch(X)
+        pred = model(X)
+
+    peak_index = tf.math.argmax(pred[:,class_index],axis=1)
+    batch_indices = []
+    for i in range(0,5):
+        column_indices = tf.range(peak_index[i]-25,peak_index[i]+25,dtype='int32')
+        row_indices = tf.keras.backend.repeat_elements(tf.constant([i]),50, axis=0)
+        full_indices = tf.stack([row_indices, column_indices], axis=1)
+        batch_indices.append([full_indices])
+        outputs = tf.keras.backend.mean(tf.gather_nd(pred[:,class_index],batch_indices),axis=2)
+
+    return tape.gradient(outputs, X)
