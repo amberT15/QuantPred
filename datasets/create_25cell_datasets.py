b4fd9b5afb00f5750b0a9a083fe575da6e74c4dd
shtoneyan <tikinbobo@gmail.com>
2021-05-18 14:24:50 -0400

    add script for making top25 cell type ATAC dataset

---
+++ datasets/create_25cell_datasets.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import subprocess
+import yaml, os, shutil
+
+# parameter grid to loop over
+# pool_window = [1, 32, 64, 128, 256]
+# input_size = [1024, 2048, 3072]
+pool_window = [1]
+input_size = [1024]
+# i = 2048
+# p = 64
+
+for i in input_size:
+    for p in pool_window:
+        print('Creating dataset using input size %i and pool window %i' % (i, p))
+        # template_path = r'ATAC_bin/template_config.yaml'
+        template_path = r'test_dataset_creation/template_config.yaml'
+
+
+        with open(template_path) as file:
+            config = yaml.load(file, Loader=yaml.FullLoader)
+        #update parameters
+        config['input']['size'] = i
+        config['input']['pool'] = p
+        config['output']['prefix'] = 'i_%i_w_%i' % (config['input']['size'], config['input']['pool'])
+        current_config = 'config.yaml'
+        #save as the config file to be used in running the preprocessing
+        with open(current_config, 'w') as file:
+            documents = yaml.dump(config, file)
+        # run dataset generation script with current parameters
+        subprocess.call('./bw_to_tfr.sh')
