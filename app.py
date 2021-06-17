1d4d842fb4540dfa532ad8196e68c2ceda6c0c1b
shtoneyan <tikinbobo@gmail.com>
2021-06-17 17:43:11 -0400

    remove mse and poisson"

--- app.py
+++ app.py
@@ -10,8 +10,14 @@ import numpy as np
 import plotly.express as px
 import flask
 import scipy.stats
+import plotly.graph_objects as go
 
-DATA_DIR='/home/shush/profile/QuantPred/datasets/ATAC_v2/grid5/pred_i_3072_w_128_bpnet_fftmse.h5'
+# y axis fixed somewhere in the Average
+# remove mse
+# dropdown menu for cell lines
+# plot extra cell lines
+
+DATA_DIR='/home/shush/profile/QuantPred/compare_training/1v2_custom_test/pred.h5'
 CELL_LINE=1
 server = flask.Flask(__name__)
 app = dash.Dash(__name__, server=server)
@@ -37,9 +43,19 @@ def get_data(pred, cell_line=1):
     dataset = h5py.File(pred, 'r')
     test_pred = dataset['test_pred'][:]
     test_y = dataset['test_y'][:]
+    coords = dataset['coords'][:]
+
+    # good_idx = np.where(test_y[:,:,cell_line].max(axis=1)>=2)
+    colors = np.array([test_y[:,:,cell_line].max(axis=1)>=2][0], dtype=int)
+    # test_pred = test_pred[good_idx][:,:,cell_line]
+    # test_y = test_y[good_idx][:,:,cell_line]
+    # coords = coords[good_idx]
+
+    pr_all = [scipy_pr(test_y[seq_n,:,cell_line], test_pred[seq_n,:,cell_line]) for seq_n in range(test_y.shape[0])]
     df = pd.DataFrame({'true_cov': test_y.mean(axis=1)[:,cell_line],
                   'pred_cov': test_pred.mean(axis=1)[:,cell_line]})
-    data = {'raw':(test_y[:,:,cell_line], test_pred[:,:,cell_line]), 'avg': df}
+    data = {'raw':(test_y[:,:,cell_line], test_pred[:,:,cell_line]),
+            'coords': coords, 'avg': df, 'pr_all': pr_all, 'peak': colors}
     return data
 
 def create_scatter(pred=DATA_DIR, cell_line=CELL_LINE):
@@ -48,11 +64,17 @@ def create_scatter(pred=DATA_DIR, cell_line=CELL_LINE):
     # x_max = np.max(data['avg']['true_cov'])
     # y_min = np.min(data['avg']['pred_cov'])
     # y_max = np.max(data['avg']['pred_cov'])
+    # data_and_pr = data['avg']['pearsonr'] = data['pr_all']
+    data['avg']['peak'] = data['peak'].astype(str)
     fig = px.scatter(data['avg'], x='true_cov',
-                     y='pred_cov', title='Average true vs pred', opacity=0.2)
-    fig.add_scatter(x=np.arange(0, 4), y=np.arange(0, 4), hoverinfo='skip', mode='lines')
+                     y='pred_cov', title='Average true vs pred', opacity=0.6,
+                     color='peak', color_continuous_scale='viridis')
+    fig.add_scatter(x=np.arange(0, 4), y=np.arange(0, 4), mode='lines', hoverinfo='skip', name='')
         # sns.scatterplot(x=test_y.mean(axis=1)[:,t], y=test_pred.mean(axis=1)[:,t], alpha=0.1, ax=axs[t,m])
         # axs[t,m].plot([0,1],[0,1], transform=axs[t,m].transAxes, color='r')
+    # fig.update_layout(coloraxis_colorbar=dict(yanchor="top", y=0, x=-1,
+    #                                       ticks="outside"))
+
     return fig
 
 
@@ -65,8 +87,7 @@ app.layout = html.Div([
     # main scatter plot panel
     html.Div([dcc.Graph(id='predictions', figure=fig)],
               style={'width': '49%', 'display': 'inline-block'}),
-    html.Div([ dcc.Graph(id='poisson'),
-              dcc.Graph(id='mse'),
+    html.Div([
               dcc.Graph(id='profile')],
               style={'width': '49%', 'display': 'inline-block'})
     #
@@ -87,27 +108,50 @@ def update_profile(hoverData, pred=DATA_DIR, cell_line=CELL_LINE):
     pr = scipy_pr(data['raw'][0][seq_n,:], data['raw'][1][seq_n,:])
     sc = scipy_sc(data['raw'][0][seq_n,:], data['raw'][1][seq_n,:])
     fig = px.line(x=np.arange(len(data['raw'][0][seq_n,:])),
-                  y = data['raw'][0][seq_n,:], title="Coverage, Pearson R={}, Spearman corr={}".format(np.around(pr,3), np.around(sc,3)))
+                  y = data['raw'][0][seq_n,:], title="Coverage, Pearson R={}, Spearman corr={}, {}".format(np.around(pr,3),
+                                                                                                           np.around(sc,3),
+                                                                                                           'chr'+'_'.join(data['coords'][:][seq_n,:].astype(np.str))))
     fig.add_scatter(x=np.arange(len(data['raw'][1][seq_n,:])),
                     y = data['raw'][1][seq_n,:],
-                    name='Predicted')
+                    name='Predicted', mode='lines', line=go.scatter.Line(color="red"))
     # fig.add_vrect( x0=0, x1=2, fillcolor="LightSalmon", opacity=0.5, layer="below", line_width=0)
     fig.update_layout()
     return fig
 
-@app.callback(
-    dash.dependencies.Output('mse', 'figure'),
-    [dash.dependencies.Input('predictions', 'hoverData')])
-def update_profile(hoverData, pred=DATA_DIR, cell_line=CELL_LINE):
-    data = get_data(pred, cell_line)
-    x = hoverData['points'][0]['x']
-    y = hoverData['points'][0]['y']
-    seq_n = np.where(data['avg']==[x, y])[0][0]
-    pr = scipy_pr(data['raw'][0][seq_n,:], data['raw'][1][seq_n,:])
-    sc = scipy_sc(data['raw'][0][seq_n,:], data['raw'][1][seq_n,:])
-    fig = px.line(x=np.arange(len(data['raw'][1][seq_n,:])),
-                    y = np_mse(data['raw'][0][seq_n,:], data['raw'][1][seq_n,:]),
-                    title='MSE')
+# @app.callback(
+#     dash.dependencies.Output('mse', 'figure'),
+#     [dash.dependencies.Input('predictions', 'hoverData')])
+# def update_profile(hoverData, pred=DATA_DIR, cell_line=CELL_LINE):
+#     data = get_data(pred, cell_line)
+#     x = hoverData['points'][0]['x']
+#     y = hoverData['points'][0]['y']
+#     seq_n = np.where(data['avg']==[x, y])[0][0]
+#     pr = scipy_pr(data['raw'][0][seq_n,:], data['raw'][1][seq_n,:])
+#     sc = scipy_sc(data['raw'][0][seq_n,:], data['raw'][1][seq_n,:])
+#     fig = px.line(x=np.arange(len(data['raw'][1][seq_n,:])),
+#                     y = np_mse(data['raw'][0][seq_n,:], data['raw'][1][seq_n,:]),
+#                     title='MSE')
+#     fig.update_traces(line_color='#60095C')
+    # fig.add_scatter(x=np.arange(len(data['raw'][1][seq_n,:])),
+    #                 y = np_poiss(data['raw'][0][seq_n,:], data['raw'][1][seq_n,:]),
+    #                 name='Poisson loss')
+    # fig.add_vrect( x0=0, x1=2, fillcolor="LightSalmon", opacity=0.5, layer="below", line_width=0)
+    # fig.update_layout()
+    # return fig
+
+# @app.callback(
+#     dash.dependencies.Output('poisson', 'figure'),
+#     [dash.dependencies.Input('predictions', 'hoverData')])
+# def update_profile(hoverData, pred=DATA_DIR, cell_line=CELL_LINE):
+#     data = get_data(pred, cell_line)
+#     x = hoverData['points'][0]['x']
+#     y = hoverData['points'][0]['y']
+#     seq_n = np.where(data['avg']==[x, y])[0][0]
+#     pr = scipy_pr(data['raw'][0][seq_n,:], data['raw'][1][seq_n,:])
+#     sc = scipy_sc(data['raw'][0][seq_n,:], data['raw'][1][seq_n,:])
+#     fig = px.line(x=np.arange(len(data['raw'][1][seq_n,:])),
+#                     y = np_poiss(data['raw'][0][seq_n,:], data['raw'][1][seq_n,:]),
+#                     title='Poisson')
     # fig.add_scatter(x=np.arange(len(data['raw'][1][seq_n,:])),
     #                 y = np_poiss(data['raw'][0][seq_n,:], data['raw'][1][seq_n,:]),
     #                 name='Poisson loss')
@@ -115,161 +159,7 @@ def update_profile(hoverData, pred=DATA_DIR, cell_line=CELL_LINE):
     fig.update_layout()
     return fig
 
-@app.callback(
-    dash.dependencies.Output('poisson', 'figure'),
-    [dash.dependencies.Input('predictions', 'hoverData')])
-def update_profile(hoverData, pred=DATA_DIR, cell_line=CELL_LINE):
-    data = get_data(pred, cell_line)
-    x = hoverData['points'][0]['x']
-    y = hoverData['points'][0]['y']
-    seq_n = np.where(data['avg']==[x, y])[0][0]
-    pr = scipy_pr(data['raw'][0][seq_n,:], data['raw'][1][seq_n,:])
-    sc = scipy_sc(data['raw'][0][seq_n,:], data['raw'][1][seq_n,:])
-    fig = px.line(x=np.arange(len(data['raw'][1][seq_n,:])),
-                    y = np_poiss(data['raw'][0][seq_n,:], data['raw'][1][seq_n,:]),
-                    title='Poisson')
-    # fig.add_scatter(x=np.arange(len(data['raw'][1][seq_n,:])),
-    #                 y = np_poiss(data['raw'][0][seq_n,:], data['raw'][1][seq_n,:]),
-    #                 name='Poisson loss')
-    # fig.add_vrect( x0=0, x1=2, fillcolor="LightSalmon", opacity=0.5, layer="below", line_width=0)
-    fig.update_layout()
-    return fig
 
-
-# df = pd.read_csv('https://plotly.github.io/datasets/country_indicators.csv')
-#
-# available_indicators = df['Indicator Name'].unique()
-#
-# app.layout = html.Div([
-#     html.H1(children='Hello Dash'),
-#     html.Div([
-#
-#         html.Div([
-#             dcc.Dropdown(
-#                 id='crossfilter-xaxis-column',
-#                 options=[{'label': i, 'value': i} for i in available_indicators],
-#                 value='Fertility rate, total (births per woman)'
-#             ),
-#             dcc.RadioItems(
-#                 id='crossfilter-xaxis-type',
-#                 options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
-#                 value='Linear',
-#                 labelStyle={'display': 'inline-block'}
-#             )
-#         ],
-#         style={'width': '49%', 'display': 'inline-block'}),
-#
-#         html.Div([
-#             dcc.Dropdown(
-#                 id='crossfilter-yaxis-column',
-#                 options=[{'label': i, 'value': i} for i in available_indicators],
-#                 value='Life expectancy at birth, total (years)'
-#             ),
-#             dcc.RadioItems(
-#                 id='crossfilter-yaxis-type',
-#                 options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
-#                 value='Linear',
-#                 labelStyle={'display': 'inline-block'}
-#             )
-#         ], style={'width': '49%', 'float': 'right', 'display': 'inline-block'})
-#     ], style={
-#         'borderBottom': 'thin lightgrey solid',
-#         'backgroundColor': 'rgb(250, 250, 250)',
-#         'padding': '10px 5px'
-#     }),
-#
-#     html.Div([
-#         dcc.Graph(
-#             id='crossfilter-indicator-scatter',
-#             hoverData={'points': [{'customdata': 'Japan'}]}
-#         )
-#     ], style={'width': '49%', 'display': 'inline-block', 'padding': '0 20'}),
-#     html.Div([
-#         dcc.Graph(id='x-time-series'),
-#         dcc.Graph(id='y-time-series'),
-#     ], style={'display': 'inline-block', 'width': '49%'}),
-#
-#     html.Div(dcc.Slider(
-#         id='crossfilter-year--slider',
-#         min=df['Year'].min(),
-#         max=df['Year'].max(),
-#         value=df['Year'].max(),
-#         marks={str(year): str(year) for year in df['Year'].unique()},
-#         step=None
-#     ), style={'width': '49%', 'padding': '0px 20px 20px 20px'})
-# ])
-#
-#
-# @app.callback(
-#     dash.dependencies.Output('crossfilter-indicator-scatter', 'figure'),
-#     [dash.dependencies.Input('crossfilter-xaxis-column', 'value'),
-#      dash.dependencies.Input('crossfilter-yaxis-column', 'value'),
-#      dash.dependencies.Input('crossfilter-xaxis-type', 'value'),
-#      dash.dependencies.Input('crossfilter-yaxis-type', 'value'),
-#      dash.dependencies.Input('crossfilter-year--slider', 'value')])
-# def update_graph(xaxis_column_name, yaxis_column_name,
-#                  xaxis_type, yaxis_type,
-#                  year_value):
-#     dff = df[df['Year'] == year_value]
-#
-#     fig = px.scatter(x=dff[dff['Indicator Name'] == xaxis_column_name]['Value'],
-#             y=dff[dff['Indicator Name'] == yaxis_column_name]['Value'],
-#             hover_name=dff[dff['Indicator Name'] == yaxis_column_name]['Country Name']
-#             )
-#
-#     fig.update_traces(customdata=dff[dff['Indicator Name'] == yaxis_column_name]['Country Name'])
-#
-#     fig.update_xaxes(title=xaxis_column_name, type='linear' if xaxis_type == 'Linear' else 'log')
-#
-#     fig.update_yaxes(title=yaxis_column_name, type='linear' if yaxis_type == 'Linear' else 'log')
-#
-#     fig.update_layout(margin={'l': 40, 'b': 40, 't': 10, 'r': 0}, hovermode='closest')
-#
-#     return fig
-#
-#
-# def create_time_series(dff, axis_type, title):
-#
-#     fig = px.scatter(dff, x='Year', y='Value')
-#
-#     fig.update_traces(mode='lines+markers')
-#
-#     fig.update_xaxes(showgrid=False)
-#
-#     fig.update_yaxes(type='linear' if axis_type == 'Linear' else 'log')
-#
-#     fig.add_annotation(x=0, y=0.85, xanchor='left', yanchor='bottom',
-#                        xref='paper', yref='paper', showarrow=False, align='left',
-#                        bgcolor='rgba(255, 255, 255, 0.5)', text=title)
-#
-#     fig.update_layout(height=225, margin={'l': 20, 'b': 30, 'r': 10, 't': 10})
-#
-#     return fig
-#
-#
-# @app.callback(
-#     dash.dependencies.Output('x-time-series', 'figure'),
-#     [dash.dependencies.Input('crossfilter-indicator-scatter', 'hoverData'),
-#      dash.dependencies.Input('crossfilter-xaxis-column', 'value'),
-#      dash.dependencies.Input('crossfilter-xaxis-type', 'value')])
-# def update_y_timeseries(hoverData, xaxis_column_name, axis_type):
-#     country_name = hoverData['points'][0]['customdata']
-#     dff = df[df['Country Name'] == country_name]
-#     dff = dff[dff['Indicator Name'] == xaxis_column_name]
-#     title = '<b>{}</b><br>{}'.format(country_name, xaxis_column_name)
-#     return create_time_series(dff, axis_type, title)
-#
-#
-# @app.callback(
-#     dash.dependencies.Output('y-time-series', 'figure'),
-#     [dash.dependencies.Input('crossfilter-indicator-scatter', 'hoverData'),
-#      dash.dependencies.Input('crossfilter-yaxis-column', 'value'),
-#      dash.dependencies.Input('crossfilter-yaxis-type', 'value')])
-# def update_x_timeseries(hoverData, yaxis_column_name, axis_type):
-#     dff = df[df['Country Name'] == hoverData['points'][0]['customdata']]
-#     dff = dff[dff['Indicator Name'] == yaxis_column_name]
-#     return create_time_series(dff, axis_type, yaxis_column_name)
-#
 
 if __name__ == '__main__':
     app.run_server(debug=True, port=8000)
