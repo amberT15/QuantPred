7e6bfdebe1a471e58dfcf39a821f3eb92eb7b181
shtoneyan <tikinbobo@gmail.com>
2021-06-30 08:52:25 -0400

    add ori y

--- get_preds.py
+++ get_preds.py
@@ -5,7 +5,7 @@ import pandas as pd
 import plotly.express as px
 import wandb
 import tensorflow as tf
-import h5py
+import h5py, os
 import numpy as np
 import plotly.express as px
 import flask
@@ -41,8 +41,11 @@ fold_name='valid'
 N=10
 
 #get data and model
+CELL_LINE = 1
 model_path = '/home/shush/profile/QuantPred/wandb/run-20210623_073933-vwq5gdk5/files/best_model.h5'
 data_dir = '/mnt/1a18a49e-9a31-4dbf-accd-3fb8abbfab2d/shush/4grid_atac/lite/random_chop/i_3072_w_1/'
+out_dir = os.path.dirname(model_path)
+
 trained_model = tf.keras.models.load_model(model_path, custom_objects={"GELU": GELU})
 trained_model.compile(optimizer="Adam", loss='mse', metrics=['mse'])
 foldset = util.make_dataset(data_dir, fold_name, util.load_stats(data_dir), coords=True, shuffle_data=False)
@@ -54,10 +57,12 @@ BIN_SIZE = l_seq // n_bins
 
 x_list = []
 y_list = []
+y_ori = []
 c_list = []
 pred_list = []
 for i, (coord, x, y) in enumerate(foldset):
     if i < N:
+        y_ori.append(y)
         x_cropped, y_cropped = custom_fit.center_crop(x,y,2048)
         y_binned = custom_fit.bin_resolution(y_cropped, BIN_SIZE)
         c_list += [str(c).strip('b\'chr').strip('\'') for c in coord.numpy()]
@@ -67,9 +72,10 @@ for i, (coord, x, y) in enumerate(foldset):
     else:
         break
 
-DATA_DICT = {'y_true': np.concatenate(y_list), 'y_pred': np.concatenate(pred_list), 'coords': c_list}
+DATA_DICT = {'y_true': np.concatenate(y_list), 'y_pred': np.concatenate(pred_list),
+            'coords': c_list, 'br_true':np.concatenate(y_ori)}
 
 
 res_list = [DATA_DICT, BIN_SIZE]
-with open(os.path.join(model_path, 'res_list.pkl'), 'wb') as f:
+with open(os.path.join(out_dir, 'res_list.pkl'), 'wb') as f:
      pickle.dump(res_list, f)
