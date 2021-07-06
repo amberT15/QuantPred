50fd909b9a61d891ce54bdf3120a995de747664b
Tang <ztang@bamdev1.elzar.cshl.edu>
2021-07-06 16:33:36 -0400

    elzar job submission code

---
+++ job_submmit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+source ~/.bash_profile
+
+NAME=bpnet_loss_test
+DIR_CODE=/grid/koo/home/ztang/QuantPred/
+d=`date +%y-%m-%d-%H-%M-%S`
+DIR_LOG=/grid/koo/home/ztang/quantlog/$NAME/$d
+SWEEP_ID=ambert/bpnet_loss_bin_test/sweeps/nnon9ynf
+export SWEEP_ID
+
+#$ -N bpnet_loss_test_4
+#$ -l m_mem_free=3G
+#$ -l gpu=1
+#$ -e /grid/koo/home/ztang/quantlog/log
+#$ -o /grid/koo/home/ztang/quantlog/log
+
+cd $DIR_CODE
+singularity exec quantpred_latest.sif python wandb_train.py $SWEEP_ID 7
