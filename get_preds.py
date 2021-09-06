a3b2b2de9a354a7aec0f486d3d7d2f2148b79f95
shtoneyan <tikinbobo@gmail.com>
2021-09-06 16:11:33 -0400

    add scripts to run prediction visualization in dash

--- get_preds.py
+++ get_preds.py
@@ -11,11 +11,13 @@ import plotly.express as px
 import flask
 import scipy.stats
 import plotly.graph_objects as go
-from modelzoo import *
-import pickle
+from modelzoo import GELU
+import h5py
 import custom_fit
 import util
-from metrics import *
+# from metrics import *
+import sys
+
 
 # y axis fixed somewhere in the Average
 # remove mse
@@ -24,45 +26,48 @@ from metrics import *
 
 
 
-fold_name='valid'
+fold_name='test'
 N=10
 
 #get data and model
-CELL_LINE = 1
-model_path = '/home/shush/profile/QuantPred/wandb/run-20210623_073933-vwq5gdk5/files/best_model.h5'
-data_dir = '/mnt/1a18a49e-9a31-4dbf-accd-3fb8abbfab2d/shush/4grid_atac/lite/random_chop/i_3072_w_1/'
-out_dir = os.path.dirname(model_path)
+CELL_LINE = int(sys.argv[1])
 
-trained_model = tf.keras.models.load_model(model_path, custom_objects={"GELU": GELU})
-trained_model.compile(optimizer="Adam", loss='mse', metrics=['mse'])
-foldset = util.make_dataset(data_dir, fold_name, util.load_stats(data_dir), coords=True, shuffle_data=False)
+run_path = sys.argv[2] #'/home/shush/profile/QuantPred/wandb/run-20210623_073933-vwq5gdk5/files/'
+data_dir = 'datasets/chr8/complete/random_chop/i_2048_w_1/'
+h5_path = os.path.join(run_path, str(CELL_LINE) + '_dash.h5')
 
-#get bin size
-n_bins = trained_model.layers[-1].output_shape[1]
-l_seq = trained_model.layers[0].input_shape[0][1]
-BIN_SIZE = l_seq // n_bins
+if not os.path.isfile(h5_path):
+    model_path = os.path.join(run_path, 'best_model.h5')
+    trained_model = tf.keras.models.load_model(model_path, custom_objects={"GELU": GELU})
+    trained_model.compile(optimizer="Adam", loss='mse', metrics=['mse'])
+    foldset = util.make_dataset(data_dir, fold_name, util.load_stats(data_dir), coords=True, shuffle=True)
 
-x_list = []
-y_list = []
-y_ori = []
-c_list = []
-pred_list = []
-for i, (coord, x, y) in enumerate(foldset):
-    if i < N:
-        y_ori.append(y)
-        x_cropped, y_cropped = custom_fit.center_crop(x,y,2048)
-        y_binned = custom_fit.bin_resolution(y_cropped, BIN_SIZE)
-        c_list += [str(c).strip('b\'chr').strip('\'') for c in coord.numpy()]
-        x_list.append(x_cropped)
-        y_list.append(y_binned[:,:,CELL_LINE])
-        pred_list.append(trained_model.predict(x_cropped)[:,:,CELL_LINE])
-    else:
-        break
+    #get bin size
+    n_bins = trained_model.layers[-1].output_shape[1]
+    l_seq = trained_model.layers[0].input_shape[0][1]
+    BIN_SIZE = l_seq // n_bins
 
-DATA_DICT = {'y_true': np.concatenate(y_list), 'y_pred': np.concatenate(pred_list),
-            'coords': c_list, 'br_true':np.concatenate(y_ori)}
+    x_list = []
+    y_list = []
+    y_ori = []
+    c_list = []
+    pred_list = []
+    for i, (coord, x, y) in enumerate(foldset):
+        if i < N:
+            y_ori.append(y)
+            x_cropped, y_cropped = custom_fit.center_crop(x,y,2048)
+            y_binned = custom_fit.bin_resolution(y_cropped, BIN_SIZE)
+            c_list += [str(c).strip('b\'chr').strip('\'') for c in coord.numpy()]
+            x_list.append(x_cropped)
+            y_list.append(y_binned[:,:,CELL_LINE])
+            pred_list.append(trained_model.predict(x_cropped)[:,:,CELL_LINE])
+        else:
+            break
 
 
-res_list = [DATA_DICT, BIN_SIZE]
-with open(os.path.join(out_dir, 'res_list.pkl'), 'wb') as f:
-     pickle.dump(res_list, f)
+
+    h = h5py.File(h5_path, 'w')
+    h.create_dataset('y_true', data= np.concatenate(y_list))
+    h.create_dataset('y_pred', data= np.concatenate(pred_list))
+    h.create_dataset('bin_size', data= np.array(BIN_SIZE))
+    h.close()
