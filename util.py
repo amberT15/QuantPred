c41b3f6f3a7d66010515d8e7e498587fe503f0f5
Ziqi (Amber) Tang <amber.zqt@gmail.com>
2021-09-30 12:12:07 -0400

    onehot encoding function

--- util.py
+++ util.py
@@ -14,12 +14,97 @@ import numpy as np
 import tensorflow as tf
 from natsort import natsorted
 import tensorflow as tf
+from collections import OrderedDict
 # import metrics
 import scipy
 import yaml
  ################################################################
  # functions for loading tfr files into tfr dataset
  ################################################################
+def dna_one_hot(seq, seq_len=None, flatten=True, n_random=False):
+    if seq_len is None:
+        seq_len = len(seq)
+        seq_start = 0
+    else:
+        if seq_len <= len(seq):
+            # trim the sequence
+            seq_trim = (len(seq) - seq_len) // 2
+            seq = seq[seq_trim : seq_trim + seq_len]
+            seq_start = 0
+        else:
+            seq_start = (seq_len - len(seq)) // 2
+
+    seq = seq.upper()
+
+    # map nt's to a matrix len(seq)x4 of 0's and 1's.
+    if n_random:
+        seq_code = np.zeros((seq_len, 4), dtype="bool")
+    else:
+        seq_code = np.zeros((seq_len, 4), dtype="float16")
+
+    for i in range(seq_len):
+        if i >= seq_start and i - seq_start < len(seq):
+            nt = seq[i - seq_start]
+            if nt == "A":
+                seq_code[i, 0] = 1
+            elif nt == "C":
+                seq_code[i, 1] = 1
+            elif nt == "G":
+                seq_code[i, 2] = 1
+            elif nt == "T":
+                seq_code[i, 3] = 1
+            else:
+                if n_random:
+                    ni = random.randint(0, 3)
+                    seq_code[i, ni] = 1
+                else:
+                    seq_code[i, :] = 0.25
+
+    # flatten and make a column vector 1 x len(seq)
+    if flatten:
+        seq_vec = seq_code.flatten()[None, :]
+
+    return seq_vec
+
+
+def hash_sequences_1hot(fasta_file, extend_len=None):
+    # determine longest sequence
+    if extend_len is not None:
+        seq_len = extend_len
+    else:
+        seq_len = 0
+        seq = ""
+        for line in open(fasta_file):
+            if line[0] == ">":
+                if seq:
+                    seq_len = max(seq_len, len(seq))
+
+                header = line[1:].rstrip()
+                seq = ""
+            else:
+                seq += line.rstrip()
+
+        if seq:
+            seq_len = max(seq_len, len(seq))
+
+    # load and code sequences
+    seq_vecs = OrderedDict()
+    seq = ""
+    for line in open(fasta_file):
+        if line[0] == ">":
+            if seq:
+                seq_vecs[header] = dna_one_hot(seq, seq_len)
+
+            header = line[1:].rstrip()
+            seq = ""
+        else:
+            seq += line.rstrip()
+
+    if seq:
+        seq_vecs[header] = dna_one_hot(seq, seq_len)
+
+    return seq_vecs
+
 def bin_resolution(y,bin_size):
     y_dim = y.shape
     y_bin = tf.math.reduce_mean(tf.reshape(y,(y_dim[0],int(y_dim[1]/bin_size),bin_size,y_dim[2])),axis = 2)
