1f0bf2845fb206cc60daa5acda8ab3385a02d339
shtoneyan <tikinbobo@gmail.com>
2021-10-13 15:36:21 -0400

    robustness pipeline script to run 1 model evaluation

--- robustness_test_pipeline.py
+++ robustness_test_pipeline.py
@@ -3,40 +3,48 @@ import h5py
 import explain
 import custom_fit
 import modelzoo
-from loss import *
 import os,json
 import util
-import pandas as pd
-import importlib
-import seaborn as sns
-import scipy
-import matplotlib.pyplot as plt
-import metrics
-import csv
+import time
 import sys
-import h5py
+import numpy as np
+from tfr_evaluate import get_run_metadata
 
-test_data = './datasets/VCF/filtered_test.h5'
-f =  h5py.File(test_data, "r")
-x = f['x_test'][()]
-y = f['y_test'][()]
-f.close()
+output_directory = util.make_dir('augmentation_robustness_output')
 
 model_path = sys.argv[1]
 id = model_path.split('-')[-1]
 
+model = modelzoo.load_model(model_path, compile=True)
+metadata = get_run_metadata(model_path)
 
-saliency_file = h5py.File('./datasets/VCF/saliency_eval_'+id+'.h5','w')
+# overlapping_testset_path = '/home/shush/profile/QuantPred/datasets/step1K_chr8_thresh2/i_3072_w_1/'
+# testset = util.make_dataset(overlapping_testset_path, 'test', util.load_stats(overlapping_testset_path),
+#                             batch_size=512, shuffle=False)
+# X_test, Y_test = util.convert_tfr_to_np(testset)
+#
+# h5f = h5py.File('/home/shush/profile/QuantPred/datasets/step1K_chr8_thresh2/testset.h5', 'w')
+# h5f.create_dataset('X', data=X_test)
+# h5f.create_dataset('Y', data=Y_test)
+# h5f.close()
 
-pred_file = h5py.File('./datasets/VCF/pred_eval_'+id+'.h5','w')
-
-model = modelzoo.load_model(model_path,compile=True)
-var_saliency,var_pred = explain.batch_robustness_test(x,y,model,batch_size = 30,visualize = False,shift_num =10)
-
-saliency_file.create_dataset('variance',data = var_saliency)
-pred_file.create_dataset('variance',data = var_pred)
-saliency_file.close()
-pred_file.close()
-
+h5f = h5py.File('/home/shush/profile/QuantPred/datasets/step1K_chr8_thresh2/testset.h5', 'r')
+X = h5f['X'][:]
+Y = h5f['Y'][:]
+start_time = time.time()
+variance_saliency, variance_pred = explain.batch_robustness_test(X, Y, model,
+                                                                batch_size=10,
+                                                                visualize=False,
+                                                                shift_num=50)
+print(np.mean(variance_pred))
+print("--- %s seconds ---" % (time.time() - start_time))
 
 tf.keras.backend.clear_session()
+
+metadata['average predictions variance'] = np.mean(variance_pred)
+metadata['std predictions variance'] = np.std(variance_pred)
+metadata['average saliency variance'] = np.mean(variance_saliency)
+metadata['std saliency variance'] = np.std(variance_saliency)
+
+print(metadata)
+metadata.to_csv(os.path.join(output_directory, id+'.csv'))
