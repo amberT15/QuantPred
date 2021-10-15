66814f148618dfcdb03222c45e48a5b47cf6bcc4
shtoneyan <tikinbobo@gmail.com>
2021-10-15 14:39:23 -0400

    add for loop over subdirs

--- loop_robustness.sh
+++ loop_robustness.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-source ~/.bash_profile
-i=1
-while [ $i -le 20 ]
-do
-	qsub -q gpu_ded.q job_submmit.sh
-	i=$(( $i+1 ))
+
+# qsub -q gpu_ded.q job_submmit.sh
+for d in $1/* ; do
+    echo "$d"
+		qsub -q gpu_ded.q submit_robustness.sh $d
 done
