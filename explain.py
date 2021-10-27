9a24b06346b81e297d92fff65c61f730863c8241
shtoneyan <tikinbobo@gmail.com>
2021-10-27 10:03:55 -0400

    add center pred return

--- explain.py
+++ explain.py
@@ -17,22 +17,27 @@ def get_center_coordinates(coord, conserve_start, conserve_end):
     start, end = np.arange(int(start), int(end))[[conserve_start,conserve_end]]
     return (chrom, start, end)
 
-def batch_pred_robustness_test(testset, sts, model, batch_size=50,
-                               shift_num=10, window_size=2048):
-    var_pred_list = []
-    pred_list = []
-    coord_list = []
-    middle_Ys = []
+def batch_pred_robustness_test(testset, sts, model, batch_size=64,
+                           shift_num=20, window_size=2048, get_preds=True):
+    predictions_and_variance = {}
+    predictions_and_variance['var'] = [] # prediction variance
+    predictions_and_variance['robust_pred'] = [] # robust predictions
+    predictions_and_variance['center_pred'] = [] # 1 shot center predictions
+    center_1K_coordinates = [] # coordinates of center 1K
+    center_ground_truth_1K = [] # center 1K ground truth base res
 
     chop_size = sts['target_length']
     center_idx = int(0.5*(chop_size-window_size))
     conserve_size = window_size*2 - chop_size
     conserve_start = chop_size//2 - conserve_size//2
     conserve_end = conserve_start + conserve_size-1
+    flanking_size = conserve_size//2
 
-    for C, seq, Y in testset:
+    for t, (C, seq, Y) in enumerate(testset):
         batch_n = seq.shape[0]
         shifted_seq,_,shift_idx = util.window_shift(seq,seq,window_size,shift_num)
+
+
         #get prediction for shifted read
         shift_pred = model.predict(shifted_seq)
         bin_size = window_size / shift_pred.shape[1]
@@ -48,16 +53,21 @@ def batch_pred_robustness_test(testset, sts, model, batch_size=50,
         #get pred 1k part
         shift_pred_1k=tf.gather_nd(shift_pred[range(shift_pred.shape[0]),:,:],crop_f_index)
         sep_pred = np.array(np.array_split(shift_pred_1k,batch_n))
-        batch_avg_predictions = sep_pred.mean(axis=1)
         var_pred = np.var(sep_pred,axis = 1)
-        var_pred_sum = np.sum(var_pred,axis = 1)
+        predictions_and_variance['var'].append(np.sum(var_pred,axis = 1))
+        if get_preds:
+            predictions_and_variance['robust_pred'].append(sep_pred.mean(axis=1))
 
-        #add var result to list
-        var_pred_list.append(var_pred_sum)
-        pred_list.append(batch_avg_predictions)
-        coord_list.append([get_center_coordinates(coord, conserve_start, conserve_end) for coord in C])
-        middle_Ys.append(Y.numpy()[:,conserve_start:conserve_end+1,:])
-    return np.concatenate(pred_list),np.concatenate(var_pred_list), np.concatenate(coord_list), np.concatenate(middle_Ys)
+            # get prediction for center crop
+            center_2K_seq = seq[:,conserve_start-flanking_size:conserve_end+1+flanking_size,:]
+            center_2K_pred = model.predict(center_2K_seq)
+            center_2K_pred_unbinned = np.repeat(center_2K_pred, window_size//center_2K_pred.shape[1], axis=1)
+            predictions_and_variance['center_pred'].append(center_2K_pred_unbinned[:,(conserve_size-flanking_size):(conserve_size+flanking_size),:])
+
+            #add ground truth center 1K coordinates and coverage
+            center_1K_coordinates.append([get_center_coordinates(coord, conserve_start, conserve_end) for coord in C])
+            center_ground_truth_1K.append(Y.numpy()[:,conserve_start:conserve_end+1,:])
+    return predictions_and_variance, center_1K_coordinates, center_ground_truth_1K
 
 def task_robustness(selected_read,model, task_idx, batch_size = 50, shift_num = 10, window_size = 2048, visualize = True, smooth_saliency = True):
     var_saliency_list = []
