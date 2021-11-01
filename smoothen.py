1a2b9aef84716fc4be41e2722a029a23c5548ce5
shtoneyan <tikinbobo@gmail.com>
2021-11-01 10:11:57 -0400

    change to no bw avg smoothing

--- smoothen.py
+++ smoothen.py
@@ -1,73 +1,58 @@
-#!/usr/bin/env python
-
-import pyBigWig, os
+import os
 import numpy as np
 import pandas as pd
-import subprocess
 from scipy import stats
-import matplotlib.pyplot as plt
-import seaborn as sns
 import shutil
-import util, sys
-import wandb
-from test_to_bw_fast import get_vals_per_range, get_list_pr
-import time
-from tqdm import tqdm
-from scipy import stats
-import sys, os
-from tqdm import tqdm
+import seaborn as sns
 import util
-import multiprocessing as mp
-from scipy.ndimage import gaussian_filter1d
+import metrics
+import yaml
+import wandb
+import tfr_evaluate
+from tqdm import tqdm
+# from test_to_bw_fast import get_config
 
-def smooth_one_binsize_one_cell_line(file, bin_size=2048, out_dir='gauss_smooth'):
-    print('Smoothening bin size {}'.format(bin_size))
-    summary_results = [] #bin_size, sigma, pearson r, cell_line
-    # grab one bin size bw (e.g. 1)
-    true_binned_dir = '/home/shush/profile/QuantPred/bin_exp/truth/'
-    pred_binned_dir = '/home/shush/profile/QuantPred/bin_exp/pred/'
-    in_bed_filename = '/home/shush/profile/QuantPred/bin_exp/truth/merged_truth_1_raw.bed'
-    subdirs = os.path.join(true_binned_dir, file)
-    print('Porcessing cell line {}'.format(file))
-    true_raw_bw_list = [os.path.join(subdirs, bw_file) for bw_file in os.listdir(subdirs) if '_{}_raw.bw'.format(bin_size) in bw_file]
-    assert len(true_raw_bw_list) == 1, 'Too many or not enough bws found in {}!'.format(subdirs)
-    true_raw_bw = true_raw_bw_list[0]
-    pred_raw_bw = true_raw_bw.replace('truth', 'pred').replace('_{}_'.format(bin_size), '_{}_{}_'.format(bin_size, bin_size))
-    assert os.path.isfile(pred_raw_bw), 'Pred bw {} not found!'.format(pred_raw_bw)
-    # load bws for truth and pred into memory
-    true_raw_np = get_vals_per_range(true_raw_bw, in_bed_filename)
-    pred_raw_np = get_vals_per_range(pred_raw_bw, in_bed_filename)
-    # compute pr
-    pr = get_list_pr(true_raw_np, pred_raw_np)
-    summary_results.append([bin_size, 0, pr, file])
-    # convert to all sigmas sequentially and compute pr-s
-    for sigma_value in tqdm([1, 5, 10, 20, 50, 100, 200, 500, 1000]):
-        print(sigma_value)
-        true_smoothened = []
-        pred_smoothened = []
-        for i in range(len(true_raw_np)):
-            true_smoothened.append(gaussian_filter1d(true_raw_np[i], sigma_value))
-            pred_smoothened.append(gaussian_filter1d(pred_raw_np[i], sigma_value))
-        smooth_pr = get_list_pr(true_smoothened, pred_smoothened)
-        summary_results.append([bin_size, sigma_value, smooth_pr, file])
-    df = pd.DataFrame(summary_results, columns=['bin_size', 'sigma', 'pearson_r', 'cell_line'])
-    df.to_csv(os.path.join(out_dir, 'bin_size_{}_{}.csv'.format(bin_size, file)))
+def smoothen(true_2K, pred_2K, eval_smooth_window_size):
+    N, L, C = true_2K.shape
+    smooth_true = np.zeros((N*L, C))
+    smooth_pred = np.zeros((N*L, C))
+    kernel = np.ones(eval_smooth_window_size) / eval_smooth_window_size
+    for cell_line in range(C):
+        smooth_true[:,cell_line] = np.convolve(true_2K[:,:,cell_line].flatten(), kernel, mode='same')
+        smooth_pred[:,cell_line] = np.convolve(pred_2K[:,:,cell_line].flatten(), kernel, mode='same')
+    return (smooth_true.reshape(N,L,C), smooth_pred.reshape(N,L,C))
+
+bin_run = {}
+for run_dir in tfr_evaluate.collect_run_dirs('BASENJI_BIN_LOSS'):
+    parameters = tfr_evaluate.get_config(run_dir)
+    if parameters['loss_fn']['value'] == 'poisson':
+        bin_run[parameters['bin_size']['value']] = run_dir
+
+testset, targets = tfr_evaluate.collect_whole_testset()
 
 
+# get performance metrics for various evaluation bin sizes
+bin_sizes = np.array([1, 32, 64, 128, 256, 512, 1024, 2048])
+complete_perf = []
+for raw_bin_size in tqdm(bin_sizes):
+    model, bin_size = tfr_evaluate.read_model(bin_run[raw_bin_size], compile_model=False)
+    assert bin_size == raw_bin_size, 'Error in bin size path dictionary!'
+    all_true, all_pred = tfr_evaluate.get_true_pred(model, bin_size, testset)
+    true_2K = np.repeat(all_true, bin_size, axis=1)
+    pred_2K = np.repeat(all_pred, bin_size, axis=1)
+    for eval_smooth_window_size in bin_sizes[bin_sizes>=raw_bin_size]:
+        print(raw_bin_size, '--->', eval_smooth_window_size)
+        if eval_smooth_window_size > raw_bin_size: # smoothen if bin size bigger than original
+            true_for_eval, pred_for_eval = smoothen(true_2K, pred_2K, eval_smooth_window_size)
+        else:
+            true_for_eval, pred_for_eval = true_2K, pred_2K
+        perf_avg = tfr_evaluate.get_performance(true_for_eval, pred_for_eval, targets, 'whole').mean()
+        one_resolution_result = [raw_bin_size, eval_smooth_window_size] + list(perf_avg.values)
+        column_names = ['raw bin size', 'smooth bin size'] + list(perf_avg.index)
+        df = pd.DataFrame(one_resolution_result).T
+        df.columns=column_names
+        complete_perf.append(df)
 
-def main():
-    out_dir = util.make_dir('gauss_smooth')
+complete_result_dataset = pd.concat(complete_perf)
 
-    true_binned_dir = '/home/shush/profile/QuantPred/bin_exp/truth/'
-    pred_binned_dir = '/home/shush/profile/QuantPred/bin_exp/pred/'
-    in_bed_filename = '/home/shush/profile/QuantPred/bin_exp/truth/merged_truth_1_raw.bed'
-    files = []
-    for file in tqdm(os.listdir(true_binned_dir)):
-        subdirs = os.path.join(true_binned_dir, file)
-        if os.path.isdir(subdirs):
-            files.append(file)
-    with mp.Pool(processes=15) as pool:
-        list(tqdm(pool.imap(smooth_one_binsize_one_cell_line, files)))
-
-if __name__ == '__main__':
-  main()
+complete_result_dataset.to_csv('smoothen_results_basenji.csv')
